3739a376a202d716ec2c0beb775c68fd6e65fa2c
ahmad bakr <bakra1217@gmail.com>
2023-06-24 22:14:48 +0300

    First commit

---
+++ Part-3/Led/Led.ino
@@ -0,0 +1,12 @@
+int led = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led,HIGH);
+}

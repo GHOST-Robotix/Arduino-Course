e4d4c015f06f67181e8b7f17e8fee57840e73da0
GHOST TECH <bakra1217@gmail.com>
2023-12-05 21:39:25 +0300

    Part-11

---
+++ Part-11/Part-11.ino
@@ -0,0 +1,48 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  lcd.setCursor(3, 0);
+  lcd.print("GHOST TECH");
+  delay(2000);
+  lcd.clear();
+  for(int i=0;i<4;i++){
+    
+    lcd.setCursor(0, 0);
+    lcd.print("Loading.");
+    delay(500);
+    lcd.clear();
+    lcd.print("Loading..");
+    delay(500);
+    lcd.clear();
+    lcd.print("Loading...");
+    delay(500);
+    lcd.clear();
+
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("Are sure to turn");
+  lcd.setCursor(0, 1);
+  lcd.print("off the Robot?");
+  lcd.blink();
+  delay(3000);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Yes           No");
+  lcd.setCursor(0, 1);
+  lcd.print("---           --");
+  lcd.blink();
+  delay(20000);
+
+
+}

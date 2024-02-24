8d7bde6d25989466b4aeb9f7cfbc6cbc555a6f6d
GHOST TECH <bakra1217@gmail.com>
2024-02-24 21:43:26 +0300

    part 15

---
+++ Part-15/Part-15.ino
@@ -0,0 +1,31 @@
+int trig = 8;
+int echo =7;
+
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(trig,OUTPUT);
+pinMode(echo,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trig,LOW);
+  delayMicroseconds(10);
+  digitalWrite(trig,HIGH);
+
+  long time = pulseIn(echo,HIGH);
+  Serial.print("time : ");
+  Serial.print(time);
+  Serial.println("");
+  delay(1000);
+  long dis = 0.017 * time ;
+   Serial.print("distance : ");
+  Serial.print(dis);
+  Serial.println(" cm");
+  delay(1000);
+
+}
